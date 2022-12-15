630c415690d73df572c83665e66746947275ad0c
rChrisb <5767@holbertonstudents.com>
2022-12-15 14:24:57 +0100

    Write a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - orts an array of integers in ascending order using the Quick sort algorithm
+ * @array: array
+ * @size: number of elements in the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
